72b644521224619e9bed6ff3b4d7037926356e4d
voltan <djvoltan@gmail.com>
2017-05-22 13:40:06 +0430

    Work on module

--- src/Controller/Front/GuideController.php
+++ src/Controller/Front/GuideController.php
@@ -19,7 +19,7 @@ use Pi\Mvc\Controller\ActionController;
  */
 class GuideController extends ActionController
 {
-    public function indexAction()
+    public function itemListAction()
     {
         // Set result
         $result = array(
@@ -40,11 +40,21 @@ class GuideController extends ActionController
                 $check = Pi::api('token', 'tools')->check($token, $module, 'api');
                 if ($check['status'] == 1) {
 
+                    $options = array();
+                    $options['page'] = $this->params('page', 1);
+                    $options['title'] = $this->params('title');
+                    $options['category'] = $this->params('category');
+                    $options['location'] = $this->params('location');
+                    $options['tag'] = $this->params('tag');
+                    $options['favourite'] = $this->params('favourite');
+                    $options['recommended'] = $this->params('recommended');
+                    $options['discount'] = $this->params('discount');
+                    $options['people'] = $this->params('people');
+                    $options['limit'] = $this->params('limit');
+                    $options['order'] = $this->params('order');
 
+                    $result = Pi::api('api', 'guide')->itemList($options);
 
-
-                    $result['status'] = 1;
-                    $result['message'] = 'Its work !';
                     return $result;
                 } else {
                     return $check;
@@ -52,8 +62,78 @@ class GuideController extends ActionController
             } else {
                 return $result;
             }
-        } else {
-            return $result;
+        }
+    }
+
+    public function itemSingleAction()
+    {
+        // Set result
+        $result = array(
+            'status' => 0,
+            'message' => '',
+        );
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('guide')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_guide']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+
+                    $id = $this->params('id');
+                    $result = Pi::api('api', 'guide')->itemSingle($id);
+
+                    return $result;
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
+        }
+    }
+
+    public function mapAction()
+    {
+        // Set result
+        $result = array(
+            'status' => 0,
+            'message' => '',
+        );
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('guide')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_guide']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+
+                    $latitude = $this->params('latitude');
+                    $longitude = $this->params('longitude');
+
+                    $result['list'] = Pi::api('api', 'guide')->itemMap($latitude, $longitude);
+                    $result['status'] = 1;
+                    $result['message'] = 'Its work !';
+
+                    return $result;
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
         }
     }
 }
