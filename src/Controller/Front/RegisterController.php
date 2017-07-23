50ef90e0ef804480d6121911c455e1da548d2098
voltan <djvoltan@gmail.com>
2017-07-23 18:30:37 +0430

    Work on api

--- src/Controller/Front/RegisterController.php
+++ src/Controller/Front/RegisterController.php
@@ -122,4 +122,93 @@ class RegisterController extends ActionController
             return $result;
         }
     }
+
+    public function editAction()
+    {
+        // Set result
+        $result = array(
+            'status' => 0,
+            'message' => __('Error on register'),
+        );
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('user')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_register']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+
+
+
+                    // Load language
+                    Pi::service('i18n')->load(array('module/user', 'default'));
+
+                    $values = array();
+                    $request = array();
+                    if (isset($_POST) && !empty($_POST)) {
+                        $request = $_POST;
+                    } if (isset($_GET) && !empty($_GET)) {
+                        $request = $_GET;
+                    }
+                    foreach ($request as $key => $value) {
+                        $key = _escape($key);
+                        $value = _strip($value);
+                        $values[$key] = $value;
+                    }
+
+                    // Set uid
+                    if (!empty($values['uid'])) {
+                        $uid = $values['uid'];
+                        unset($values['uid']);
+                    } else {
+                        return $result;
+                    }
+
+                    // Fields
+                    $fields = Pi::api('user', 'user')->getFields($uid, 'profile');
+                    // Set just needed fields
+                    foreach (array_keys($values) as $key) {
+                        if (!in_array($key, array_keys($fields))) {
+                            unset($values[$key]);
+                        }
+                    }
+                    // From user module
+                    $values['last_modified'] = time();
+                    if (isset($values['first_name']) || isset($values['last_name'])) {
+                        $values['name'] = $values['first_name'] . ' ' . $values['last_name'];
+                    }
+
+                    $status = Pi::api('user', 'user')->updateUser($uid, $values);
+                    if ($status == 1) {
+                        Pi::service('event')->trigger('user_update', $uid);
+                    }
+
+                    $result = array(
+                        'status' => 1,
+                        'message' => __('Your account information update'),
+                    );
+
+
+
+
+
+
+
+                    return $result;
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
+        } else {
+            return $result;
+        }
+    }
 }
