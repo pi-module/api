7b0c7f7140db50c7b630269fa9e2effd970cf043
voltan <djvoltan@gmail.com>
2018-02-15 10:50:16 +0330

    Work on api

--- src/Controller/Front/VideoController.php
+++ src/Controller/Front/VideoController.php
@@ -17,7 +17,7 @@ use Pi\Mvc\Controller\ActionController;
  */
 class VideoController extends ActionController
 {
-    public function sendAction()
+    public function videoListAction()
     {
         // Set result
         $result = [
@@ -38,9 +38,93 @@ class VideoController extends ActionController
                 $check = Pi::api('token', 'tools')->check($token, $module, 'api');
                 if ($check['status'] == 1) {
 
+                    $params                = [];
+                    $params['page']        = $this->params('page', 1);
+                    $params['title']       = $this->params('title');
+                    $params['category']    = $this->params('category');
+                    $params['tag']         = $this->params('tag');
+                    $params['favourite']   = $this->params('favourite');
+                    $params['recommended'] = $this->params('recommended');
+                    $params['limit']       = $this->params('limit');
+                    $params['order']       = $this->params('order');
+                    $params['channel']     = $this->params('channel');
+                    $result                = Pi::api('api', 'video')->videoList($params);
+                    $result['status']      = 1;
+                    return $result;
 
-                    $result['status']  = 1;
-                    $result['message'] = 'Its work !';
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
+        } else {
+            return $result;
+        }
+    }
+
+    public function videoSingleAction()
+    {
+        // Set result
+        $result = [
+            'status'  => 0,
+            'message' => '',
+        ];
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token  = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('video')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_video']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+
+                    $params           = [];
+                    $params['id']     = $this->params('id');
+                    $params['slug']   = $this->params('slug');
+                    $result           = Pi::api('api', 'video')->videoSingle($params);
+                    $result['status'] = 1;
+
+                    return $result;
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
+        } else {
+            return $result;
+        }
+    }
+
+    public function categoryListAction()
+    {
+        // Set result
+        $result = [
+            'status'  => 0,
+            'message' => '',
+        ];
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token  = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('video')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_video']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+                    $params           = [];
+                    $result           = Pi::api('api', 'video')->categoryList($params);
+                    $result['status'] = 1;
                     return $result;
                 } else {
                     return $check;
