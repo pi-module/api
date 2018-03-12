e3e94513de8599786a84a41b2d8af40e983da861
voltan <djvoltan@gmail.com>
2018-03-12 15:42:23 +0330

    Work on api

--- src/Controller/Front/VideoController.php
+++ src/Controller/Front/VideoController.php
@@ -136,4 +136,39 @@ class VideoController extends ActionController
             return $result;
         }
     }
+
+    public function setAccessAction()
+    {
+        // Set result
+        $result = [
+            'status'  => 0,
+            'message' => '',
+        ];
+        // Set template
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        // Get info from url
+        $module = $this->params('module');
+        $token  = $this->params('token');
+        // Check module
+        if (Pi::service('module')->isActive('video')) {
+            // Check config
+            $config = Pi::service('registry')->config->read($module);
+            if ($config['active_video']) {
+                // Check token
+                $check = Pi::api('token', 'tools')->check($token, $module, 'api');
+                if ($check['status'] == 1) {
+                    $params        = [];
+                    $params['id']  = $this->params('id');
+                    $params['uid'] = $this->params('uid');
+                    return Pi::api('api', 'video')->setAccess($params);
+                } else {
+                    return $check;
+                }
+            } else {
+                return $result;
+            }
+        } else {
+            return $result;
+        }
+    }
 }
